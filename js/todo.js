db9fee29190aa55e4627a1e0ed7d2ef4d7509d2f
ZHOUAD <Jing690518>
2018-07-19 18:29:23 +0800

    finished

--- js/todo.js
+++ js/todo.js
@@ -21,5 +21,53 @@ $(document)
             return uuid;
         }
 
-        // code to be implemented
+
+        $("#button").click(function(){
+            $("ol").append("<li ><input type='checkbox' class=''>"+$(".input-text").val()+"</li>");
+            $("#ListItem").val("");
+        })
+
+
+        $('input').focus(function () {
+            $(this).val('');
+        });;
+        $(document).on("dblclick", "li", function () {
+                 const li = $(this);
+            $(this).attr("contentEditable",true);
+            $(this).keypress(function (event) {
+                         if(event.which===13){
+                                 li.attr("contentEditable",false);
+                             }
+                     })
+             });
+
+        $(document).on('click','input[type=checkbox]',function(){
+            $(this).parent()
+                .toggleClass('checked');
+        })
+
+        $("a[data-filter='active']").click(function(){
+                 $("a[class='selected']").removeClass("selected");
+                 $(this).addClass("selected");
+                 $("ol>li:not(.checked)").css("display","");
+                 $(".checked").css("display","none");
+               })
+
+
+           $("a[data-filter='all']").click(function(){
+                 $("a[class='selected']").removeClass("selected");
+                 $(this).addClass("selected");
+                 $("li").css("display","");
+               })
+
+
+
+             $("a[data-filter='complete']").click(function(){
+                   $("a[class='selected']").removeClass("selected");
+                   $(this).addClass("selected");
+                   $(".checked").css("display","");
+                   $("ol>li:not(.checked)").css("display","none");
+                 })
+
+
     });
